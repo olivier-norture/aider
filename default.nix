6966a1e99f65d6673b5b8289d47b7f4f0649f15a
Olivier Norture <olivier@norture.fr>
2025-04-29 09:38:30 +0200

    use aider from nix

--- default.nix
+++ default.nix
@@ -3,27 +3,19 @@ with pkgs;
 
 pkgs.mkShell rec {
   buildInputs = with pkgs; [
-    stdenv.cc.cc.lib
-    python312
-    python312Packages.pip
-    python312Packages.virtualenvwrapper
+    aider-chat-with-playwright
   ];
 
   shellHook = ''
+    source $HOME/aider/.env
     [ -z "OPENAI_API_KEY" ] && echo "Please set OPENAI_API_KEY"
-    [ -z "OPENAI_API_BASE" ] && echo "Please set OPENAI_API_KEY"
-
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath buildInputs}"
-    export TMPDIR=/$HOME/aider  && export VENV=/$HOME/aider
-    virtualenv $VENV
-    source $VENV/bin/activate
-    pip install pyside2
+    [ -z "OPENAI_API_BASE" ] && echo "Please set OPENAI_API_BASE"
 
     function myAider() {
       aider \
-      -- vim\
+      --vim \
       --model-settings-file=$HOME/config.yaml \
-      [ -z "AIDER_MODEL" ] && $1 || "--model $AIDER_MODEL" $1
+      --model $AIDER_MODEL
     }
 
     alias aider=myAider
